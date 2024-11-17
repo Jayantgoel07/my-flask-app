42860c1dcd000e16800a725f2d96cd13547f0357
Jayant Goel <136612182+Jayantgoel07@users.noreply.github.com>
2024-11-17 21:28:47 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/') 
     assert response.status_code == 200
-    expected_text = 'GFGGGGGG'
+    expected_text = 'Hello! there my name is Jayant Goel'
     assert expected_text.encode() in response.data
